7f69038fa8f572e27d1be706d985ef84cfde3945
Muliro <mulirokhaemba@gmail.com>
2023-08-30 21:26:39 +0300

    perfect

--- binary_trees.h
+++ binary_trees.h
@@ -83,5 +83,6 @@ int heap_extract(heap_t **root);
 int *heap_to_sorted_array(heap_t *heap, size_t *size);
 int binary_tree_is_avl(const binary_tree_t *tree);
 int binary_tree_balance(const binary_tree_t *tree);
+int binary_tree_is_perfect(const binary_tree_t *tree);
 
 #endif /* __BINARY_TREES_H__ */
